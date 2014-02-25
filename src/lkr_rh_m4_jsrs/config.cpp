4f93b6aa6d0224e82ed8d9766f876a084d943c85
Lukas Kropatschek <lukas@kropatschek.net>
2014-02-25 21:49:36 +0100

    seperated rh m4 jsrs and sound replacement

---
+++ src/lkr_rh_m4_jsrs/config.cpp
@@ -0,0 +1,124 @@
+/*
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
+a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+*/
+
+class CfgPatches
+{
+	class lkr_rh_m4_jsrs
+	{
+		units[]= {};
+		weapons[]= {};
+		requiredVersion=1.0;
+		requiredAddons[]=
+		{
+			"RH_m4_cfg"
+		};
+	};
+};
+class Mode_SemiAuto;
+class Mode_Burst;
+class Mode_FullAuto;
+class CfgWeapons
+{
+	class Rifle_Base_F;
+	class RH_ar10: Rifle_Base_F
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"RH_m4\sound\ar10.wss", 1.1220185, 1, 130};
+			begin2[]={"RH_m4\sound\ar10.wss", 1.1220185, 0.85000002, 130};
+			begin3[]={"RH_m4\sound\ar10.wss", 1.1220185, 1.2, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			begin1[]={"RH_m4\sound\ar10.wss", 1.1220185, 1, 130};
+			begin2[]={"RH_m4\sound\ar10.wss", 1.1220185, 0.85000002, 130};
+			begin3[]={"RH_m4\sound\ar10.wss", 1.1220185, 1.2, 130};
+			soundBegin[]={"begin1", 0.34, "begin2", 0.33000001, "begin3", 0.33000001};
+		};
+	};
+	class RH_m4: Rifle_Base_F
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\RH_m4\sound\m4_fire1", 1.2589254, 1, 130};
+			begin2[]={"\RH_m4\sound\m4_fire2", 1.2589254, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class Burst: Mode_Burst
+		{
+			begin1[]={"\RH_m4\sound\m4_fire1", 1.5848932, 1, 130};
+			begin2[]={"\RH_m4\sound\m4_fire2", 1.5848932, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			begin1[]={"\RH_m4\sound\m4_fire1", 1.5848932, 1, 130};
+			begin2[]={"\RH_m4\sound\m4_fire2", 1.5848932, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+	};
+	class RH_M16a1: RH_m4
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\RH_m4\sound\m16a1_fire1", 1.4125376, 1, 130};
+			begin2[]={"\RH_m4\sound\m16a1_fire2", 1.4125376, 1.2, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			begin1[]={"\RH_m4\sound\m16a1_fire1", 1.4125376, 1, 130};
+			begin2[]={"\RH_m4\sound\m16a1_fire2", 1.4125376, 1.2, 130};
+			soundBegin[]={ "begin1", 0.5, "begin2", 0.5};
+		};
+	};
+	class RH_M16A2: RH_m4
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\RH_m4\sound\m16_fire1", 1.7782789, 1, 130};
+			begin2[]={"\RH_m4\sound\m16_fire2", 1.7782789, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class Burst: Mode_Burst
+		{
+			begin1[]={"\RH_m4\sound\m16_fire1", 1.7782789, 1, 130};
+			begin2[]={"\RH_m4\sound\m16_fire2", 1.7782789, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+	};
+	class RH_M16A4: RH_M4_ris
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\RH_m4\sound\m16_fire1", 1.7782789, 1, 130};
+			begin2[]={"\RH_m4\sound\m16_fire2", 1.7782789, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+		class Burst: Mode_Burst
+		{
+			begin1[]={"\RH_m4\sound\m16_fire1", 1.7782789, 1, 130};
+			begin2[]={"\RH_m4\sound\m16_fire2", 1.7782789, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+	};
+	class RH_Mk12mod1: RH_M16A4
+	{
+		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\RH_m4\sound\m16_fire1", 1.7782789, 1, 130};
+			begin2[]={"\RH_m4\sound\m16_fire2", 1.7782789, 1, 130};
+			soundBegin[]={"begin1", 0.5, "begin2", 0.5};
+		};
+	};
+};
--- src/lkr_rh_m4_sound/config.cpp
+++ src/lkr_rh_m4_sound/config.cpp
@@ -1,6 +1,6 @@
 /*
-This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send 
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
 a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 */
 
@@ -8,16 +8,13 @@ class CfgPatches
 {
 	class lkr_rh_m4_sound
 	{
-		units[]={};
-		weapons[]={};
-		magazines[]={};
+		units[]= {};
+		weapons[]= {};
 		requiredVersion=1.0;
 		requiredAddons[]=
 		{
-			"RH_m4_cfg",
-			"mas_weapons",
-			"mas_weapons_m4",
-			"A3_Weapons_F"
+			"lkr_rh_m4_jsrs",
+			"mas_weapons_m4"
 		};
 	};
 };
@@ -27,111 +24,54 @@ class Mode_FullAuto;
 class CfgWeapons
 {
 	class Rifle_Base_F;
+	class RH_ar10: Rifle_Base_F
+	{
+		class Single: Mode_SemiAuto
+		{
+			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
+			begin2[]={"\mas_us_rifle\sounds\m16_s2", 0.9, 1, 130};
+			begin3[]={"\mas_us_rifle\sounds\m16_s3", 0.9, 1, 130};
+			begin4[]={"\mas_us_rifle\sounds\m16_s4", 0.9, 1, 130};
+			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
+			begin2[]={"\mas_us_rifle\sounds\m16_s2", 0.9, 1, 130};
+			begin3[]={"\mas_us_rifle\sounds\m16_s3", 0.9, 1, 130};
+			begin4[]={"\mas_us_rifle\sounds\m16_s4", 0.9, 1, 130};
+			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
+		};
+	};
 	class RH_m4: Rifle_Base_F
 	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
 		class Single: Mode_SemiAuto
 		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
+			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1, 130};
+			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1, 130};
+			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1, 130};
+			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1, 130};
 			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
 		};
 		class Burst: Mode_Burst
 		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-	};
-	class RH_M4_ris: RH_m4
-	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
-		class Single: Mode_SemiAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-		class Burst: Mode_Burst
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-	};
-
-	class RH_M4A1_ris: RH_M4_ris
-	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
-		class Single: Mode_SemiAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
+			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1, 130};
+			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1, 130};
+			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1, 130};
+			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1, 130};
 			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
 		};
 		class FullAuto: Mode_FullAuto
 		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-	};
-	class RH_M4_ris_M203;
-	class RH_M4A1_ris_M203: RH_M4_ris_M203
-	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
-		class Single: Mode_SemiAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-		class FullAuto: Mode_FullAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-	};
-	class RH_M4sbr: RH_M4_ris
-	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
-		class Single: Mode_SemiAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};
-		class FullAuto: Mode_FullAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1,	130};
-			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1,	130};
-			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1,	130};
-			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1,	130};
+			begin1[]={"\mas_us_rifle\sounds\m4_s1", 1, 1, 130};
+			begin2[]={"\mas_us_rifle\sounds\m4_s2", 1, 1, 130};
+			begin3[]={"\mas_us_rifle\sounds\m4_s3", 1, 1, 130};
+			begin4[]={"\mas_us_rifle\sounds\m4_s4", 1, 1, 130};
 			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
 		};
 	};
 	class RH_M16a1: RH_m4
 	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
 		class Single: Mode_SemiAuto
 		{
 			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
@@ -151,7 +91,6 @@ class CfgWeapons
 	};
 	class RH_M16A2: RH_m4
 	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
 		class Single: Mode_SemiAuto
 		{
 			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
@@ -171,7 +110,6 @@ class CfgWeapons
 	};
 	class RH_M16A4: RH_M4_ris
 	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
 		class Single: Mode_SemiAuto
 		{
 			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
@@ -187,20 +125,11 @@ class CfgWeapons
 			begin3[]={"\mas_us_rifle\sounds\m16_s3", 0.9, 1, 130};
 			begin4[]={"\mas_us_rifle\sounds\m16_s4", 0.9, 1, 130};
 			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
-		};  
-	};
-	class RH_M16A3: RH_M16A4
-	{
-		jsrs_soundeffect = "JSRS2_Distance_Effects_M16";
-		class Single: Mode_SemiAuto
-		{
-			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
-			begin2[]={"\mas_us_rifle\sounds\m16_s2", 0.9, 1, 130};
-			begin3[]={"\mas_us_rifle\sounds\m16_s3", 0.9, 1, 130};
-			begin4[]={"\mas_us_rifle\sounds\m16_s4", 0.9, 1, 130};
-			soundBegin[]={"begin1", 0.25, "begin2", 0.25, "begin3", 0.25, "begin4", 0.25};
 		};
-		class FullAuto: Mode_FullAuto
+	};
+	class RH_Mk12mod1: RH_M16A4
+	{
+		class Single: Mode_SemiAuto
 		{
 			begin1[]={"\mas_us_rifle\sounds\m16_s1", 0.9, 1, 130};
 			begin2[]={"\mas_us_rifle\sounds\m16_s2", 0.9, 1, 130};
