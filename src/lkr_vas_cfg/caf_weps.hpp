76893734147f781d4c0e986bdf2d58821d41543f
Lukas Kropatschek <lukas@kropatschek.net>
2014-04-04 15:28:39 +0200

    removed lkr_vas_cfg -> seperate mod

--- src/lkr_vas_cfg/caf_weps.hpp
+++
@@ -1,8 +0,0 @@
-/*
-This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send 
-a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-*/
-
-#define LKR_VAS_CAF_WEPS "caf_AK47", "caf_AK74", "caf_ak74gl", "caf_pkm", "caf_rpg7", "caf_rpk74", "caf_svd", "caf_Strela"
-#define LKR_VAS_CAF_MAGS "CAF_100Rnd_762x54_PKM", "caf_10Rnd_762x54_SVD", "CAF_30Rnd_545x39_AK", "CAF_30Rnd_762x39_AK","CAF_75rnd_545x39_RPK", "caf_AA_rocket", "caf_OG7", "caf_PG7V"
